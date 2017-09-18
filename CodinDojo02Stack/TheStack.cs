0b372bb4e368a70b9404f44d9c9f3eb0749ceb23
Ian <ianburrows.sa@gmail.com>
2017-09-18 11:25:38 +0200

    push implemented

---
+++ CodinDojo02Stack/TheStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinDojo02Stack
+{
+    class TheStack<T>
+    {
+        private StackElement<T> currentElement;
+
+        public void push(T item)
+        {
+            if (currentElement == null)
+            {
+                currentElement = new StackElement<T>() { ValueOfElement = item, successor = null };
+            }
+            else
+            {
+                StackElement<T> temp = new StackElement<T>() { ValueOfElement = item, successor = currentElement };
+                currentElement = temp;
+            }
+        }
+    }
+}
